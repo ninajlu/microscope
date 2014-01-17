524e5d07c43f102d6ca6424b4a45cad6767f3162
Nina Lu <ninajlu@gmail.com>
2014-01-16 23:15:02 -0500

    got basic profile working

--- server/publications.js
+++ server/publications.js
@@ -5,7 +5,9 @@ Meteor.publish('posts', function(options) {
 Meteor.publish('singlePost', function(id) {
   return id && Posts.find(id);
 });
-
+Meteor.publish('singleUser', function(id) {
+  return id && Meteor.users.find(id);
+});
 
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
@@ -16,4 +18,10 @@ Meteor.publish('notifications', function() {
 });
 Meteor.publish('tags', function () {
   return Meteor.tags.find({});
+});
+Accounts.onCreateUser(function(options, user) {
+  // We're enforcing at least an empty profile object to avoid needing to check
+  // for its existence later.
+  user.profile = options.profile ? options.profile : {};
+  return user;
 });
