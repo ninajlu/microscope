66dd1c3651e27ffceaed41932e49885e08661d1b
Nina Lu <ninajlu@gmail.com>
2014-03-19 21:31:46 -0400

    edits

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -5,6 +5,9 @@ Template.postsList.helpers({
   }
 });
 Template.postsList.rendered= function(){
+	if(Router.current().limit()>5){
+		window.scrollTo(0,document.body.scrollHeight);
+	}
 $('.typeahead').typeahead({
     name: 'Some name',
     local: ['test', 'abc', 'def']
